11ddb1bd0ba67a4e51c61552e592f420d562e19a
Ripandeepsidhu <kaursidhuripandeep@gmail.com>
2023-12-07 10:55:43 +0100

    coded by mistake in master

--- src/components/QuizView.js
+++ src/components/QuizView.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./QuizView.css";
 
 const QuizView = ({questions}) => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+   
+    const [clickedOption,setClickedOption]= useState(0);
+    const changeQuestion = () => {
+        if(currentQuestion<questions.length-1)
+        setCurrentQuestion(currentQuestion+1)
+    }
     return (
     <>
         <div className="question">
             <div className="question-number">
-                <span> Question 1 / {questions.length} </span>
+                <span> {currentQuestion+1}. / {questions.length} </span>
             </div>
             <div className="question-text"> 
-                {questions.question}
+                {questions[currentQuestion].question}
             </div> 
                 {questions.image && (
             <img src={questions.image} alt={`Question ${1}`} />
@@ -18,10 +25,11 @@ const QuizView = ({questions}) => {
 
 
         <div className="answer">
-            {questions[0].answer.map(({ text, isCorrect })=> (
-                <button key={text}>{text}</button>
+            {questions[currentQuestion].answer.map(({ text, isCorrect })=> (
+                <button onClick={() => setClickedOption(i+1)} key={text}>{text}</button>
             )) }
         </div>
+        <input onClick={changeQuestion} type="button" value= "Next" id="next-button"/>
     </>
     )
 };
