63ef86bd8aa801930c9692b6ac0b3c1e00f375bf
Ripandeepsidhu <kaursidhuripandeep@gmail.com>
2023-12-07 15:07:34 +0100

    functionality added to next button and images

--- src/components/ScoreView.js
+++ src/components/ScoreView.js
@@ -2,10 +2,7 @@ import React from "react";
 import "./ScoreView.css";
 
 const ScoreView = () => {
-    const [score, setScore] = useState(0);
-    const updateScore =()=> {
-        
-    }
+    
     return (
         <div>
             <p> You score 1 out of 6 </p>
