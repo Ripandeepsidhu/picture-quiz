c6ed03ce7980634398ab8042d1ebf4bb59b2c6db
Katja Ollila <katjahfs@hotmail.com>
2023-12-08 16:03:37 +0100

    more cod for register user

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,18 +1,25 @@
+import {useState} from "react";
+import axios from "axios";
 import Card from "./Card.js"
+
 const Register = () => {
+const [username, setUsername] = {useState}(""); 
+const [email, setEmail] = {useState}("");
+const [password, setPassword] = {useState}("");
+
     return (
     <div className="container">
         <Card>
             <form classname="form-container">
                 <h1>Register User</h1>
                 <label>
-                    <input type="text" placeholder="Name" />
+                    <input type="text" placeholder="Name" value={username} />
                 </label>
                 <label>
-                    <input type="text" placeholder="Email" />
+                    <input type="text" placeholder="Email" value={email} />
                 </label>
                 <label>
-                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password" value={password} />
                 </label>         
                 <button className="btn">
                     <p>Register</p>
