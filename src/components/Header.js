5d771e20432d5c47737e6c7aaf7dc3100014d9c8
Ripandeepsidhu <kaursidhuripandeep@gmail.com>
2023-12-11 18:02:56 +0100

    changes in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,18 @@
-
+import React from "react";
  import { Link } from "react-router-dom";
 import "./Header.css"
  const Header=()=>{
 return( <nav>
+ <Link className="title" to="/"> Quiz..</Link>
     <ul>
     <li>
-       <Link to="/"> Quiz..</Link>
+       <Link to="/register"> Register user</Link>
     </li>
     <li>
-       <Link to="/Register"> Register user</Link>
-    </li>
-    <li>
-        <Link to="/Login">Log in</Link>
+        <Link to="/login">Log in</Link>
     </li>
     </ul>
+    <i className="fas fa-toggle-on"></i>
     </nav>)
  }
  export default Header;
